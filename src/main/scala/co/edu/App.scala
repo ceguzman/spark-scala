c87174ddfe3f71d8843e2bc0d54e0bb8b7874b0d
Carlos Esteban Guzman Baquero <ceguzman582@misena.edu.co>
2023-02-20 15:08:37 -0500

    commit initial

---
+++ src/main/scala/co/edu/App.scala
@@ -0,0 +1,15 @@
+package co.edu
+
+/**
+ * @author ${user.name}
+ */
+object App {
+
+  def main(args: Array[String]) {
+    println("Hello World!")
+    println(calcularSuma(5,3))
+  }
+
+  def calcularSuma(num1: Int, num2: Int) = num1 + num2
+
+}
